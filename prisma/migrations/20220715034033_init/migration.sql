15bd40e06ff0139a508f993ed7204bb2c9e62cf9
yuya.yoshioka <yuya3330930@yahoo.co.jp>
2022-07-15 12:52:38 +0900

    feat: Prisma導入

---
+++ prisma/migrations/20220715034033_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Todo` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `title` VARCHAR(20) NOT NULL,
+    `text` VARCHAR(200) NOT NULL,
+    `isCompleted` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
